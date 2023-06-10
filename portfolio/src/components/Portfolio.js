6395bfcbe26155284e1434c81b2f425d5caa462f
MissHeather <heather.hawkes808@gmail.com>
2023-06-09 18:23:21 -0700

    clean up

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -8,7 +8,6 @@ function Portfolio() {
         {/* Project 1 */}
         <div className="project">
           <h3>Project 1</h3>
-          <p>Description of Project 1</p>
           <a className="white-link" href="https://example.com/project1" target="_blank" rel="noopener noreferrer">
             <img src="/path/to/project1.jpg" alt="Project 1" />
           </a>
@@ -20,7 +19,6 @@ function Portfolio() {
         {/* Project 2 */}
         <div className="project">
           <h3>Project 2</h3>
-          <p>Description of Project 2</p>
           <a className="white-link" href="https://example.com/project2" target="_blank" rel="noopener noreferrer">
             <img src="/assets/wdsched.jpeg" alt="Project 2" />
           </a>
@@ -32,7 +30,6 @@ function Portfolio() {
         {/* Project 3 */}
         <div className="project">
           <h3>Project 3</h3>
-          <p>Description of Project 3</p>
           <a className="white-link" href="https://example.com/project3" target="_blank" rel="noopener noreferrer">
             <img src="/assets/smapi.png" alt="Project 3" />
           </a>
@@ -44,7 +41,6 @@ function Portfolio() {
         {/* Project 4 */}
         <div className="project">
           <h3>Project 4</h3>
-          <p>Description of Project 4</p>
           <a className="white-link" href="https://example.com/project4" target="_blank" rel="noopener noreferrer">
             <img src="/path/to/project4.jpg" alt="Project 4" />
           </a>
@@ -56,7 +52,6 @@ function Portfolio() {
         {/* Project 5 */}
         <div className="project">
           <h3>Project 5</h3>
-          <p>Description of Project 5</p>
           <a className="white-link" href="https://example.com/project5" target="_blank" rel="noopener noreferrer">
             <img src="/path/to/project5.jpg" alt="Project 5" />
           </a>
@@ -68,7 +63,6 @@ function Portfolio() {
         {/* Project 6 */}
         <div className="project">
           <h3>Project 6</h3>
-          <p>Description of Project 6</p>
           <a className="white-link" href="https://example.com/project6" target="_blank" rel="noopener noreferrer">
             <img src="/path/to/project6.jpg" alt="Project 6" />
           </a>
